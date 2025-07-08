29b2fe91ce7ee6def646b170223ed950bf27e90e
Michael Rudolph <michael.rudolph@uni-due.de>
2025-07-08 15:07:40 +0000

    Some structural changes/cleanup

--- main.py
+++
@@ -1,8 +0,0 @@
-import bitstream_bindings
-
-
-
-if __name__ == "__main__":
-    config_path = "./configs/test_config.yaml"
-    bitstream_path = "./data/test_config.yaml"
-    transcode(bitstream_path, config_path)
