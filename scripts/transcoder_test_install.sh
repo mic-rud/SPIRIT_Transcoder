b5039999268ea3d178f8d6d57b4ca8ad44654d84
Michael Rudolph <michael.rudolph@uni-due.de>
2025-08-08 14:38:20 +0000

    Tweaks on install script

--- scripts/transcoder_test_install.sh
+++ scripts/transcoder_test_install.sh
@@ -1,17 +1,32 @@
+#!/bin/bash
+
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y git
+sudo apt install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release \
+  software-properties-common \
+  git
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 sudo apt update
 sudo apt install docker-ce -y
+
 sudo systemctl start docker
 sudo systemctl enable docker
 
-git clone git@github.com:mic-rud/SPIRIT_Transcoder.git
+#Git repo 
+git clone https://github.com/mic-rud/SPIRIT_Transcoder.git
 cd SPIRIT_Transcoder
 
+#Docker building
 docker build -t transcoder -f ./docker/Transcoder.Dockerfile .
-docker run --rm -it -v ./data:/sapapp/data:z -v ./src:/app/src:z -v ./configs:/app/configs:z -v ./docker:/app/docker:z transcoder
+
+#Docker running
 docker run --rm -it \
   -v ./data:/app/data:z \
   -v ./configs:/app/configs:z \
